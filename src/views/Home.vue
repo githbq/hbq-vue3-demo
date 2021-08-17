c422743a439e2f4697a4fe90cd6d8f3742e9ef3b
hubiqing <hubiqing03@kuaishou.com>
2021-08-17 21:41:49 +0800

    ok

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,7 @@
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { Options, Vue } from "vue-class-component"; 
 
 @Options({
   components: { 
