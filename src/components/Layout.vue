a8eb6b94b006c40a900d6e977dae537a28a418af
hubiqing <hubiqing03@kuaishou.com>
2021-08-17 21:13:14 +0800

    layout

---
+++ src/components/Layout.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-container style="height: 100vh; border: 1px solid #eee">
+    <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+        left
+    </el-aside>
+
+    <el-container>
+      <el-header style="text-align: left; font-size: 12px"> header </el-header>
+
+      <el-main> main </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  data() {
+    const item = {
+      date: "2016-05-02",
+      name: "王小虎",
+      address: "上海市普陀区金沙江路 1518 弄",
+    };
+    return {
+      tableData: Array(20).fill(item),
+    };
+  },
+};
+</script>
+<style>
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
+}
+
+.el-aside {
+  color: #333;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
+import ElementPlus from 'element-plus';
+import 'css-reset-and-normalize/less/reset-and-normalize.less';
+import 'element-plus/lib/theme-chalk/index.css';
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(ElementPlus).use(store).use(router).mount("#app");
